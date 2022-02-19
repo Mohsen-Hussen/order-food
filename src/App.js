f2ccd47c82dc4956fda2b81c7fce04b009b93863
Mohsen Hussen <mohsenhussen8@gmail.com>
2022-02-19 22:46:23 +0200

    add cadr & meal components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals"
 
 const App = () => {
 	return (
 		<>
 			<Header />
+      <Meals />
 		</>
 	);
 };
