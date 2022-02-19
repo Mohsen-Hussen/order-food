0dab7546bcc3693c6bd46f92d6810def188bd61a
Mohsen Hussen <mohsenhussen8@gmail.com>
2022-02-19 23:51:10 +0200

    add Modal component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import Header from "./components/Layout/Header";
-import Meals from "./components/Meals/Meals"
+import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
 
 const App = () => {
 	return (
 		<>
+			<Cart />
 			<Header />
-      <Meals />
+			<main>
+				<Meals />
+			</main>
 		</>
 	);
 };
--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,16 +1,17 @@
 import React from "react";
 import classes from "./Cart.module.css";
+import Modal from "../UI/Modal";
 
 const Cart = (props) => {
 	const cartItems = (
 		<ul className={classes["cart-items"]}>
 			{[{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }].map((item) => (
-				<li>{item.name}</li>
+				<li key={item.id}>{item.name}</li>
 			))}
 		</ul>
 	);
 	return (
-		<div>
+		<Modal>
 			{cartItems}
 			<div className={classes.total}>
 				<span>Total Amount</span>
@@ -20,7 +21,7 @@ const Cart = (props) => {
 				<button className={classes["button--alt"]}>Close</button>
 				<button className={classes.button}>Order</button>
 			</div>
-		</div>
+		</Modal>
 	);
 };
 
