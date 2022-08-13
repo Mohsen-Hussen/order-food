7bacbea8e7eb0c5763fe8cdb68ca583bdcd5a621
mohsen-hussen <mohsenhussen8@gmail.com>
2022-08-14 00:03:16 +0200

    add cart form validation

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,12 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import classes from "./Cart.module.css";
 import Modal from "../UI/Modal";
 import CartItem from "./CartItem";
 import CartContext from "../../store/cart-context";
+import Checkout from "./Checkout";
 
 const Cart = (props) => {
+	const [isCheckout, setIsCheckout] = useState(false);
 	const cartCtx = useContext(CartContext);
 	const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
 	const hasItems = cartCtx.items.length > 0;
@@ -13,6 +15,9 @@ const Cart = (props) => {
 	};
 	const cartItemAddHandler = (item) => {
 		cartCtx.addItem({ ...item, amount: 1 });
+	};
+	const orderHandler = () => {
+		setIsCheckout(true);
 	};
 
 	const cartItems = (
@@ -30,6 +35,19 @@ const Cart = (props) => {
 		</ul>
 	);
 
+	const modalActions = (
+		<div className={classes.actions}>
+			<button className={classes["button--alt"]} onClick={props.onClose}>
+				Close
+			</button>
+			{hasItems && (
+				<button className={classes.button} onClick={orderHandler}>
+					Order
+				</button>
+			)}
+		</div>
+	);
+
 	return (
 		<Modal onClose={props.onClose}>
 			{cartItems}
@@ -37,12 +55,8 @@ const Cart = (props) => {
 				<span>Total Amount</span>
 				<span>{totalAmount}</span>
 			</div>
-			<div className={classes.actions}>
-				<button className={classes["button--alt"]} onClick={props.onClose}>
-					Close
-				</button>
-				{hasItems && <button className={classes.button}>Order</button>}
-			</div>
+			{isCheckout && <Checkout onCancel={props.onClose} />}
+			{!isCheckout && modalActions}
 		</Modal>
 	);
 };
