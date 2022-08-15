062e9e91b27485e29e37338aeefe0485eab7313a
mohsen-hussen <mohsenhussen8@gmail.com>
2022-08-15 05:12:28 +0200

    sned post request by orders to firebase

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -20,6 +20,19 @@ const Cart = (props) => {
 		setIsCheckout(true);
 	};
 
+	const submitOrderHandler = (userData) => {
+		fetch(
+			"https://react-order-food-bc861-default-rtdb.firebaseio.com/orders.json",
+			{
+				method: "POST",
+				body: JSON.stringify({
+					user: userData,
+					orderedItems: cartCtx.items,
+				}),
+			}
+		);
+	};
+	
 	const cartItems = (
 		<ul className={classes["cart-items"]}>
 			{cartCtx.items.map((item) => (
@@ -55,7 +68,9 @@ const Cart = (props) => {
 				<span>Total Amount</span>
 				<span>{totalAmount}</span>
 			</div>
-			{isCheckout && <Checkout onCancel={props.onClose} />}
+			{isCheckout && (
+				<Checkout onConfirm={submitOrderHandler} onCancel={props.onClose} />
+			)}
 			{!isCheckout && modalActions}
 		</Modal>
 	);
